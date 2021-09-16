0705d76c3648430a8ca281556386dae1dff105b4
LeeeSeongwon <leeseongwon13@gmail.com>
2021-09-17 01:56:20 +0900

    [스프링 핵심 원리 이해2] 스프링으로 전환하기

--- core/src/main/java/hello/core/AppConfig.java
+++ core/src/main/java/hello/core/AppConfig.java
@@ -7,23 +7,34 @@ import hello.core.member.MemoryMemberRepository;
 import hello.core.order.OrderService;
 import hello.core.order.OrderServiceImpl;
 import hello.core.discount.FixDiscountPolicy;
-import hello.core.discount.DiscountPolicy;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import hello.core.discount.DiscountPolicy;
+import hello.core.discount.RateDiscountPolicy;
+
+@Configuration
 public class AppConfig {
 
+    @Bean
     public MemberService memberService(){
         return new MemberServiceImpl(memberRepository());
     }
 
-    private MemberRepository memberRepository(){
+    @Bean
+    public MemberRepository memberRepository(){
         return new MemoryMemberRepository();
     }
 
+    @Bean
     public OrderService orderService() {
         return new OrderServiceImpl(memberRepository(), discountPolicy());
     }
 
-    private DiscountPolicy discountPolicy(){
-        return new FixDiscountPolicy();
+    @Bean
+    public DiscountPolicy discountPolicy(){
+        // return new FixDiscountPolicy();
+        return new RateDiscountPolicy();
     }
 }
