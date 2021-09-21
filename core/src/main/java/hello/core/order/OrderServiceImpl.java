e732c207abeedb73b7188c4eb11d98346e379d13
LeeeSeongwon <leeseongwon13@gmail.com>
2021-09-21 23:55:24 +0900

    [의존관계 자동 주입] annotation 직접 만들기

--- core/src/main/java/hello/core/order/OrderServiceImpl.java
+++ core/src/main/java/hello/core/order/OrderServiceImpl.java
@@ -1,33 +1,26 @@
 package hello.core.order;
 
+import lombok.RequiredArgsConstructor;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
+import hello.core.annotation.MainDiscountPolicy;
 import hello.core.discount.DiscountPolicy;
 import hello.core.member.Member;
 import hello.core.member.MemberRepository;
 
 @Component
+// @RequiredArgsConstructor
 public class OrderServiceImpl implements OrderService {
 
     // interface에만 의존하도록 변경 -> DIP 지킴
     private final MemberRepository memberRepository;
     private final DiscountPolicy discountPolicy;
 
-    // -- 수정자 주입 --
-    // @Autowired
-    // public void setMemberRepository(MemberRepository memberRepository) {
-    // this.memberRepository = memberRepository;
-    // }
-
-    // @Autowired
-    // public void setDiscountPolicy(DiscountPolicy discountPolicy) {
-    // this.discountPolicy = discountPolicy;
-    // }
-
-    // -- 생성자 주입 --
     @Autowired
-    public OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
+    public OrderServiceImpl(MemberRepository memberRepository, @MainDiscountPolicy DiscountPolicy discountPolicy) {
         // final 변수 있으면 이렇게 무조건 생성자 만들어 주어야 함
         this.memberRepository = memberRepository;
         this.discountPolicy = discountPolicy;
