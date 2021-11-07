8a27f59585a0d5445f25397898a5a961431f3d11
helloWon <leeseongwon13@gmail.com>
2021-11-07 16:31:41 +0900

    [상품 수정]

--- mvc1-2/src/main/java/hello/itemservice/web/basic/BasicItemController.java
+++ mvc1-2/src/main/java/hello/itemservice/web/basic/BasicItemController.java
@@ -66,7 +66,7 @@ public class BasicItemController {
         return "basic/item";
     }
 
-    @PostMapping("/add")
+    // @PostMapping("/add")
     public String addItemV3(@ModelAttribute Item item) {
         itemRepository.save(item);
         return "basic/item";
@@ -76,6 +76,19 @@ public class BasicItemController {
     public String addItemV4(Item item) { // ModelAttribute 생략가능
         itemRepository.save(item);
         return "basic/item";
+    }
+
+    @GetMapping("/{itemId}/edit")
+    public String editForm(@PathVariable Long itemId, Model model) {
+        Item item = itemRepository.findById(itemId);
+        model.addAttribute("item", item);
+        return "basic/editForm";
+    }
+
+    @PostMapping("/{itemId}/edit")
+    public String edit(@PathVariable Long itemId, @ModelAttribute Item item) {
+        itemRepository.update(itemId, item);
+        return "redirect:/basic/items/{itemId}";
     }
 
     /**
