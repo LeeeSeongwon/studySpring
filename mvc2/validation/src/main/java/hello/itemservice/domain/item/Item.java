a437576c54daf5a9b5b5984b02c23280d95f6b17
helloWon <leeseongwon13@gmail.com>
2021-11-19 00:23:38 +0900

    [검증2 - Bean Validation] Bean Validation - 오브젝트 오류

--- mvc2/validation/src/main/java/hello/itemservice/domain/item/Item.java
+++ mvc2/validation/src/main/java/hello/itemservice/domain/item/Item.java
@@ -5,10 +5,14 @@ import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 
 import org.hibernate.validator.constraints.Range;
+import org.hibernate.validator.constraints.ScriptAssert;
 
 import lombok.Data;
 
 @Data
+// @ScriptAssert(lang = "javascript", script = "_this.price * _this.quantity >=
+// 10000", message = "총합이 10000원 넘게 입력해주세요")
+// 너무 과해서 사용 잘 안함
 public class Item {
 
     private Long id;
