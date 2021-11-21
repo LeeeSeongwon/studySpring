e7a968d4c8677255364cd9c2fce5a54ffde4b016
helloWon <leeseongwon13@gmail.com>
2021-11-21 15:50:32 +0900

    [로그인 처리1 - 쿠키, 세션] 서블릿 HTTP 세션 사용하기

--- mvc2/login/src/main/java/hello/login/web/HomeController.java
+++ mvc2/login/src/main/java/hello/login/web/HomeController.java
@@ -4,11 +4,14 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+import javax.websocket.SessionException;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.CookieValue;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.SessionAttribute;
 
 import hello.login.domain.member.Member;
 import hello.login.domain.member.MemberRepository;
@@ -41,7 +44,7 @@ public class HomeController {
         return "loginHome";
     }
 
-    @GetMapping("/")
+    // @GetMapping("/")
     public String homeLoginV2(HttpServletRequest request, Model model) {
         // 세션 관리자에 저장된 회원 정보 조회
         Member member = (Member) sessionManager.getSession(request);
@@ -51,6 +54,40 @@ public class HomeController {
             return "home";
         }
         model.addAttribute("member", member);
+        return "loginHome";
+    }
+
+    // @GetMapping("/")
+    public String homeLoginV3(HttpServletRequest request, Model model) {
+
+        HttpSession session = request.getSession(false);
+        if (session == null) {
+            return "home";
+        }
+
+        Member loginMember = (Member) session.getAttribute(SessionConst.LOGIN_MEMBER);
+
+        // 로그인
+        if (loginMember == null) {
+            return "home";
+        }
+
+        // 세션이 유지되면 이동
+        model.addAttribute("member", loginMember);
+        return "loginHome";
+    }
+
+    @GetMapping("/")
+    public String homeLoginV3Spring(
+            @SessionAttribute(name = SessionConst.LOGIN_MEMBER, required = false) Member loginMember, Model model) {
+
+        // 로그인
+        if (loginMember == null) {
+            return "home";
+        }
+
+        // 세션이 유지되면 이동
+        model.addAttribute("member", loginMember);
         return "loginHome";
     }
 }
