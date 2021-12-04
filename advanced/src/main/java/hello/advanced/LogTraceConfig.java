d99146e5811a69f82bce9bf66dba6b3a620275c7
helloWon <leeseongwon13@gmail.com>
2021-12-04 22:24:05 +0900

    [쓰레드 로컬 - ThreadLocal] ThreadLocal

--- advanced/src/main/java/hello/advanced/LogTraceConfig.java
+++ advanced/src/main/java/hello/advanced/LogTraceConfig.java
@@ -5,13 +5,15 @@ import org.springframework.context.annotation.Configuration;
 
 import hello.advanced.trace.logtrace.FieldLogTrace;
 import hello.advanced.trace.logtrace.LogTrace;
+import hello.advanced.trace.logtrace.ThreadLocalLogTrace;
 
 @Configuration
 public class LogTraceConfig {
 
     @Bean
     public LogTrace logTrace() {
-        return new FieldLogTrace(); // singleton으로 등록됨
+        // return new FieldLogTrace(); // singleton으로 등록됨
+        return new ThreadLocalLogTrace();
     }
 
 }
