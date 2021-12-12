99f7258c85d9d4d1274233f980e697b75c5072a1
helloWon <leeseongwon13@gmail.com>
2021-12-12 11:25:32 +0900

    [빈 후처리기] 스프링이 제공하는 빈 후처리기

--- advanced/proxy/src/main/java/hello/proxy/ProxyApplication.java
+++ advanced/proxy/src/main/java/hello/proxy/ProxyApplication.java
@@ -13,12 +13,13 @@ import org.springframework.context.annotation.Import;
 // import hello.proxy.config.v2_dynamicproxy.DynamicProxyFilterConfig;
 // import hello.proxy.config.v3_proxyfactory.advice.ProxyFactoryConfigV1;
 // import hello.proxy.config.v3_proxyfactory.advice.ProxyFactoryConfigV2;
-import hello.proxy.config.v4_postprocessor.BeanPostProcessorConfig;
+// import hello.proxy.config.v4_postprocessor.BeanPostProcessorConfig;
+import hello.proxy.config.v5_autoproxy.AutoProxyConfig;
 import hello.proxy.trace.logtrace.LogTrace;
 import hello.proxy.trace.logtrace.ThreadLocalLogTrace;
 
 // @Import({ AppV1Config.class, AppV2Config.class })
-@Import(BeanPostProcessorConfig.class)
+@Import(AutoProxyConfig.class)
 @SpringBootApplication(scanBasePackages = "hello.proxy.app") // 주의
 public class ProxyApplication {
 
