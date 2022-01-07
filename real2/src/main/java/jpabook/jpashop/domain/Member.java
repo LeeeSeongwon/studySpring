43cbae333ec6a9f4fcd1af8e96309732d4d6a205
helloWon <leeseongwon13@gmail.com>
2022-01-08 03:49:10 +0900

    [API 개발 기본] 회원 조회 API

--- real2/src/main/java/jpabook/jpashop/domain/Member.java
+++ real2/src/main/java/jpabook/jpashop/domain/Member.java
@@ -11,6 +11,8 @@ import javax.persistence.Id;
 import javax.persistence.OneToMany;
 import javax.validation.constraints.NotEmpty;
 
+// import com.fasterxml.jackson.annotation.JsonIgnore;
+
 import lombok.Getter;
 import lombok.Setter;
 
@@ -30,6 +32,7 @@ public class Member {
     @Embedded
     private Address address;
 
+    // @JsonIgnore
     @OneToMany(mappedBy = "member") // readonly
     private List<Order> orders = new ArrayList<>();
 
