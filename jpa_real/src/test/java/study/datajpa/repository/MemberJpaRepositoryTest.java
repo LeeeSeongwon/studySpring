c6f0e2d49a2ee136532f5729992cb54004cf6268
helloWon <leeseongwon13@gmail.com>
2022-01-11 06:43:44 +0900

    [프로젝트 환경설정] 스프링 데이터 JPA와 DB 설정, 동작확인

---
+++ jpa_real/src/test/java/study/datajpa/repository/MemberJpaRepositoryTest.java
@@ -0,0 +1,32 @@
+package study.datajpa.repository;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import javax.transaction.Transactional;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Rollback;
+
+import study.datajpa.entity.Member;
+
+@SpringBootTest
+@Transactional // data 변경은 모두 transaction 안에서 실행되어야함
+@Rollback(false)
+public class MemberJpaRepositoryTest {
+
+    @Autowired
+    MemberJpaRepository memberJpaRepository;
+
+    @Test
+    public void testMember() {
+        Member member = new Member("memberA");
+        Member savedMember = memberJpaRepository.save(member);
+        Member findMember = memberJpaRepository.find(savedMember.getId());
+
+        assertThat(findMember.getId()).isEqualTo(member.getId());
+        assertThat(findMember.getUsername()).isEqualTo(member.getUsername());
+        assertThat(findMember).isEqualTo(member); // JPA 엔티티 동일성 보장
+    }
+}
