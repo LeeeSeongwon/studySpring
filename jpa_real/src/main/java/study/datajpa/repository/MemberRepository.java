ea765bb6f6b01e8037a4e6e84c94e7ea6fbf74a1
helloWon <leeseongwon13@gmail.com>
2022-01-15 14:18:38 +0900

    [쿼리 메소드 기능] @Query, 리포지토리 메소드에 쿼리 정의하기

--- jpa_real/src/main/java/study/datajpa/repository/MemberRepository.java
+++ jpa_real/src/main/java/study/datajpa/repository/MemberRepository.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+// import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
 import study.datajpa.entity.Member;
@@ -11,6 +12,9 @@ import study.datajpa.entity.Member;
 public interface MemberRepository extends JpaRepository<Member, Long> {
     List<Member> findByUsernameAndAgeGreaterThan(String username, int age);
 
-    @Query(name = "Member.findByUsername")
+    // @Query(name = "Member.findByUsername") 생략가능
     List<Member> findByUsername(@Param("username") String username);
+
+    @Query("select m from Member m where m.username = :username and m.age = :age") // 제일 많이 쓰인다
+    List<Member> findUser(@Param("username") String username, @Param("age") int age);
 }
