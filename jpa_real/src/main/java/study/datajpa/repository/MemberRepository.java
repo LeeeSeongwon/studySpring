82f28937e297cf59fd77de17032936453514df14
helloWon <leeseongwon13@gmail.com>
2022-01-15 14:34:24 +0900

    [쿼리 메소드 기능] @Query, 값, DTO 조회하기, 파라미터 바인딩

--- jpa_real/src/main/java/study/datajpa/repository/MemberRepository.java
+++ jpa_real/src/main/java/study/datajpa/repository/MemberRepository.java
@@ -7,9 +7,11 @@ import org.springframework.data.jpa.repository.Query;
 // import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
+import study.datajpa.dto.MemberDto;
 import study.datajpa.entity.Member;
 
 public interface MemberRepository extends JpaRepository<Member, Long> {
+
     List<Member> findByUsernameAndAgeGreaterThan(String username, int age);
 
     // @Query(name = "Member.findByUsername") 생략가능
@@ -17,4 +19,14 @@ public interface MemberRepository extends JpaRepository<Member, Long> {
 
     @Query("select m from Member m where m.username = :username and m.age = :age") // 제일 많이 쓰인다
     List<Member> findUser(@Param("username") String username, @Param("age") int age);
+
+    @Query("select m.username from Member m")
+    List<String> findUsernameList();
+
+    @Query("select new study.datajpa.dto.MemberDto(m.id, m.username, t.name) " + "from Member m join m.team t")
+    List<MemberDto> findMemberDto();
+
+    // in절
+    @Query("select m from Member m where m.username in :names")
+    List<Member> findByNames(@Param("names") List<String> names);
 }
