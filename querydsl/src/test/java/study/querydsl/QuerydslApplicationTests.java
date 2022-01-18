e74b745725aac7879b082fb13189af40d6168b53
helloWon <leeseongwon13@gmail.com>
2022-01-18 22:44:37 +0900

    [프로젝트 환경설정] config application.yml

--- querydsl/src/test/java/study/querydsl/QuerydslApplicationTests.java
+++ querydsl/src/test/java/study/querydsl/QuerydslApplicationTests.java
@@ -9,6 +9,7 @@ import com.querydsl.jpa.impl.JPAQueryFactory;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Commit;
 import org.springframework.transaction.annotation.Transactional;
 
 import study.querydsl.entity.Hello;
@@ -16,6 +17,7 @@ import study.querydsl.entity.QHello;
 
 @SpringBootTest
 @Transactional
+@Commit // rollback 방지
 class QuerydslApplicationTests {
 
 	@Autowired
